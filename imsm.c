caa4d968bed3ac49f1524e9e6b9922dc5c7eba0d
Paul Khuong <pvk@pvk.ca>
2019-12-28 12:27:56 -0500

    imsm: tweak freelist layout to make it easier to inline code without exposing internals

--- imsm.c
+++ imsm.c
@@ -13,6 +13,34 @@ struct imsm_slab_magazine {
         struct imsm_entry *entries[SLAB_MAGAZINE_SIZE];
 };
 
+/*
+ * Returns a pointer to the allocation cache in a magazine.
+ */
+static struct imsm_entry **
+cache_of_magazine(struct imsm_slab_magazine *magazine)
+{
+
+        assert(magazine != NULL);
+        return &magazine->entries[0];
+}
+
+/*
+ * Inverses `cache_of_magazine`.
+ */
+static struct imsm_slab_magazine *
+magazine_of_cache(struct imsm_entry **entries)
+{
+        const size_t offset = __builtin_offsetof(struct imsm_slab_magazine, entries);
+        char *base = (void *)entries;
+
+        assert(entries != NULL);
+        return (void *)(base - offset);
+}
+
+/*
+ * Returns an empty magazine, either from the slab's list cache of empty magazines,
+ * of via `calloc`.
+ */
 static struct imsm_slab_magazine *
 slab_get_empty_magazine(struct imsm_slab *slab)
 {
@@ -27,39 +55,64 @@ slab_get_empty_magazine(struct imsm_slab *slab)
         return calloc(1, sizeof(*ret));
 }
 
-static void
-slab_flush(struct imsm_slab *slab)
+/*
+ * Replaces a slab's current freeing magazine.
+ */
+static inline void
+slab_refresh_current_freeing(struct imsm_slab *slab)
 {
-        struct imsm_slab_magazine *full = slab->current_freeing;
+        struct imsm_slab_magazine *empty;
 
-        assert(slab->current_free_index == SLAB_MAGAZINE_SIZE &&
-            "slab_flush must only be called on full magazines");
-        full->next = slab->freelist;
-        slab->freelist = full->next;
+        assert(slab->current_freeing == NULL);
 
-        slab->current_freeing = slab_get_empty_magazine(slab);
-        slab->current_free_index = 0;
+        empty = slab_get_empty_magazine(slab);
+        slab->current_freeing = cache_of_magazine(empty);
+        slab->current_free_index = SLAB_MAGAZINE_SIZE;
         return;
 }
 
+/*
+ * Flushes the current full freeing magazine to the freelist, and
+ * replaces it with a new empty magazine.
+ */
+static void
+slab_flush(struct imsm_slab *slab)
+{
+        struct imsm_slab_magazine *full;
+
+        assert(slab->current_free_index == 0 &&
+            "slab_flush must only be called on full magazines");
+
+        full = magazine_of_cache(slab->current_freeing);
+        full->next = slab->freelist;
+        slab->freelist = full->next;
+
+        slab->current_freeing = NULL;
+        slab_refresh_current_freeing(slab);
+        return;
+}
+
+/*
+ * Pushes `entry` to the freelist.
+ */
 static void
 slab_add_free(struct imsm_slab *slab, struct imsm_entry *entry)
 {
 
-        if (slab->current_free_index >= SLAB_MAGAZINE_SIZE)
+        slab->current_freeing[--slab->current_free_index] = entry;
+        if (slab->current_free_index == 0)
                 slab_flush(slab);
-        slab->current_freeing->entries[slab->current_free_index++] = entry;
         return;
 }
 
 static void
 slab_init_freelist(struct imsm_slab *slab)
 {
-        const uintptr_t arena = (uintptr_t)slab->arena;
+        char *const arena = slab->arena;
         const size_t elsize = slab->element_size;
         const size_t nelem = slab->arena_size / elsize;
 
-        slab->current_freeing = slab_get_empty_magazine(slab);
+        slab_refresh_current_freeing(slab);
         for (size_t i = nelem; i --> 0; ) {
                 struct imsm_entry *to_free;
 
@@ -68,6 +121,17 @@ slab_init_freelist(struct imsm_slab *slab)
                 slab_add_free(slab, to_free);
         }
 
+        /*
+         * Confirm that we setup a valid slab.
+         */
+        assert(slab->current_alloc_index == 0);
+        assert(slab->current_free_index > 0 &&
+            slab->current_free_index <= SLAB_MAGAZINE_SIZE);
+
+        /* The slab's allocation cache is initially empty. */
+        assert(slab->current_allocating == NULL);
+        assert(slab->current_freeing != NULL);
+        assert(slab->empty == NULL);
         return;
 }
 
--- imsm_internal.h
+++ imsm_internal.h
@@ -9,17 +9,23 @@ struct imsm_slab {
         void *arena;
         size_t arena_size;
         size_t element_size;
+        uint32_t current_alloc_index;
+        uint32_t current_free_index;
         /*
-         * The first two magazines are caches, one for allocations and
-         * another for deallocations.
+         * These two arrays are caches populated or consumed down from
+         * current_*_index to 0.  Once the item at zero is populated
+         * or consumed, the cache must be recycled.
          *
+         * `current_allocating` may be NULL if we're out of slab items;
+         * `current_freeing` must always be a valid non-full magazine.
+         */
+        struct imsm_entry **current_allocating;
+        struct imsm_entry **current_freeing;
+
+        /*
          * The other two are intrusive linked stacks of full magazines
          * (freelist) or of empty ones (empty).
          */
-        uint32_t current_alloc_index;
-        uint32_t current_free_index;
-        struct imsm_slab_magazine *current_allocating;
-        struct imsm_slab_magazine *current_freeing;
         struct imsm_slab_magazine *freelist;
         struct imsm_slab_magazine *empty;
 };

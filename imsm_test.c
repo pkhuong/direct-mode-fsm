99bd863fcead9a8f39bb5e2afd38651a12a44b05
Paul Khuong <pvk@pvk.ca>
2019-12-28 13:40:11 -0500

    imsm_test: add slab exhaustion coverage

--- imsm_test.c
+++ imsm_test.c
@@ -49,11 +49,70 @@ slab_get_put()
         return;
 }
 
+void
+slab_get_put_tight()
+{
+        static struct echo_imsm small_echo;
+        static struct echo_state buf[2];
+        struct echo_state *state0, *state1, *state2;
+
+        /* Make sure we handle small arenas. */
+        IMSM_INIT(&small_echo, header, buf, sizeof(buf),
+                  echo_poll);
+
+        state0 = IMSM_GET(&small_echo);
+        state1 = IMSM_GET(&small_echo);
+        state2 = IMSM_GET(&small_echo);
+        printf("%p %p %p\n", state0, state1, state2);
+
+        assert(state0 != NULL);
+        assert(state1 != NULL);
+        assert(state2 == NULL);
+
+        IMSM_PUT(&small_echo, state2);
+        IMSM_PUT(&small_echo, state1);
+
+        state2 = IMSM_GET(&small_echo);
+        assert(state2 == state2);
+
+        IMSM_PUT(&small_echo, state2);
+        IMSM_PUT(&small_echo, state0);
+
+        state0 = IMSM_GET(&small_echo);
+        state1 = IMSM_GET(&small_echo);
+        state2 = IMSM_GET(&small_echo);
+        printf("%p %p %p\n", state0, state1, state2);
+
+        assert(state0 != NULL);
+        assert(state1 != NULL);
+        assert(state2 == NULL);
+        return;
+}
+
+void
+slab_get_empty()
+{
+        static struct echo_imsm small_echo;
+        static char buf[sizeof(struct echo_state) - 1];
+        struct echo_state *state;
+
+        /* Make sure we handle empty arenas. */
+        IMSM_INIT(&small_echo, header, buf, sizeof(buf),
+                  echo_poll);
+
+        state = IMSM_GET(&small_echo);
+        printf("%p\n", state);
+        assert(state == NULL);
+        return;
+}
+
 int
 main()
 {
 
         init();
         slab_get_put();
+        slab_get_put_tight();
+        slab_get_empty();
         return 0;
 }

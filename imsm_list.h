b09b8c72bbf795d29332b4965daf5967dbcdc7d2
Paul Khuong <pvk@pvk.ca>
2019-12-31 12:38:07 -0500

    imsm_list: more const correctness, reorder crap

--- imsm_list.h
+++ imsm_list.h
@@ -4,6 +4,34 @@
 #include <stddef.h>
 #include <stdint.h>
 
+struct imsm_list_cache;
+
+void imsm_list_cache_deinit(struct imsm_list_cache *);
+
+void imsm_list_cache_recycle(struct imsm_list_cache *);
+
+inline size_t imsm_list_size(void **);
+
+inline bool imsm_list_set_size(void **, size_t);
+
+inline size_t imsm_list_capacity(void **);
+
+inline uint64_t* imsm_list_aux(void **);
+
+inline bool imsm_list_push(void **, void *ptr, uint64_t aux);
+
+inline void **imsm_list_get(struct imsm_list_cache *, size_t capacity);
+
+inline void imsm_list_put(struct imsm_list_cache *, void **list);
+
+#define imsm_list_foreach(VAR, BUF)                                      \
+        for (__typeof__(**(BUF))                                         \
+             *const *VAR##_list_ = (BUF),                                \
+             *const *VAR##_sentinel_ = VAR##_list_ + imsm_list_size(VAR##_list_), \
+             *VAR;                                                       \
+             VAR##_list_ < VAR##_sentinel_ && ((VAR = *VAR##_list_), 1); \
+             VAR##_list_++)
+
 /*
  * We represent state lists stretchy buffers.  This type-erased
  * implementation only offers lists of `void *`.
@@ -33,53 +61,27 @@ struct imsm_list_cache {
         struct imsm_list_cache_head uncached_active;
 };
 
-void imsm_list_cache_deinit(struct imsm_list_cache *);
-
-void imsm_list_cache_recycle(struct imsm_list_cache *);
-
-inline size_t imsm_list_size(void **);
-
-inline bool imsm_list_set_size(void **, size_t);
-
-inline size_t imsm_list_capacity(void **);
-
-inline uint64_t* imsm_list_aux(void **);
-
-inline bool imsm_list_push(void **, void *ptr, uint64_t aux);
-
-inline void **imsm_list_get(struct imsm_list_cache *, size_t capacity);
-
-inline void imsm_list_put(struct imsm_list_cache *, void **list);
-
-#define imsm_list_foreach(VAR, BUF)                                      \
-        for (__typeof__(**(BUF))                                         \
-             **VAR##_list_ = (BUF),                                      \
-             **VAR##_sentinel_ = VAR##_list_ + imsm_list_size(VAR##_list_), \
-             *VAR;                                                       \
-             VAR##_list_ < VAR##_sentinel_ && ((VAR = *VAR##_list_), 1); \
-             VAR##_list_++)
-
-#define imsm_list_size(BUF)                              \
-        ({                                               \
-                __typeof__(**(BUF)) **imsm_buf_ = (BUF); \
-                (imsm_list_size)((void **)imsm_buf_);    \
+#define imsm_list_size(BUF)                                    \
+  ({                                                           \
+                __typeof__(**(BUF)) *const *imsm_buf_ = (BUF); \
+                (imsm_list_size)((void **)imsm_buf_);          \
         })
 
-#define imsm_list_set_size(BUF, SIZE)                                   \
-        ({                                                              \
-                __typeof__(**(BUF)) **imsm_buf_ = (BUF);                \
-                (imsm_list_set_size)((void **)imsm_buf_, (SIZE));       \
+#define imsm_list_set_size(BUF, SIZE)                             \
+        ({                                                        \
+                __typeof__(**(BUF)) **imsm_buf_ = (BUF);          \
+                (imsm_list_set_size)((void **)imsm_buf_, (SIZE)); \
         })
 
 #define imsm_list_capacity(BUF)                                 \
         ({                                                      \
-                __typeof__(**(BUF)) **imsm_buf_ = (BUF);        \
+                __typeof__(**(BUF)) *const *imsm_buf_ = (BUF);  \
                 (imsm_list_capacity)((void **)imsm_buf_);       \
         })
 
 #define imsm_list_aux(BUF)                                      \
         ({                                                      \
-                __typeof__(**(BUF)) **imsm_buf_ = (BUF);        \
+                __typeof__(**(BUF)) *const *imsm_buf_ = (BUF);  \
                 (imsm_list_aux)((void **)imsm_buf_);            \
         })
 
